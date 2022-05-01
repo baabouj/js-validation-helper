5366d28c06acfea48ac81db7ead934be5bb54da9
Omar Baabouj <baabouj.dev@gmail.com>
2022-05-01 12:14:39 +0000

    :ok_hand: IMPROVE: bundle package using rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+      },
+      {
+        file: "dist/index.mjs",
+        format: "es",
+        exports: "named",
+      },
+    ],
+    plugins: [terser()],
+  },
+];
