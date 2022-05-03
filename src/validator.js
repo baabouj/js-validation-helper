7a8811639eb52e8aed6bbdb1a880d84d6995fa21
Omar Baabouj <baabouj.dev@gmail.com>
2022-05-03 22:24:28 +0000

    :ok_hand: IMPROVE: custom error messages

--- src/validator.js
+++ src/validator.js
@@ -1,18 +1,32 @@
 import { rules } from "./rules.js";
 import { messages } from "./messages.js";
 
-const check = (value, rule) => {
+const checkRule = (value, rule, ...args) => {
+  if (!rules[rule]) {
+    throw new Error(`Rule '${rule}' does not exist`);
+  }
+  return rules[rule](value, ...args);
+};
+
+const check = (value, fieldRules) => {
   let errors = [];
-  rule.split("|").forEach((rule) => {
-    const [key, ...args] = rule.split(":");
-    if (!rules.hasOwnProperty(key)) {
-      throw new Error(`${key} is not a valid rule`);
-    }
-    if (!rules[key](value, ...args)) {
-      let message = messages[key];
-      args.forEach((arg, index) => {
-        message = message.replace(`{${index}}`, arg);
-      });
+  if (!rules.array(fieldRules)) {
+    fieldRules = fieldRules.split("|");
+  }
+  fieldRules.forEach((fieldRule) => {
+    let [rule, message] = rules.array(fieldRule)
+      ? fieldRule
+      : [fieldRule, messages[fieldRule]];
+    const [ruleName, ...ruleArgs] = rule.split(":");
+
+    if (!checkRule(value, ruleName, ...ruleArgs)) {
+      if (!message) {
+        message = messages[ruleName];
+
+        ruleArgs.forEach((arg, index) => {
+          message = message.replace(`{${index}}`, arg);
+        });
+      }
       errors.push(message);
     }
   });
@@ -22,9 +36,9 @@ const check = (value, rule) => {
 const validate = (data, rules) => {
   let errors = {};
   for (let field in rules) {
-    const error = check(data[field], rules[field]);
-    if (error) {
-      errors = { ...errors, [field]: error };
+    const errorsForField = check(data[field], rules[field]);
+    if (errorsForField) {
+      errors[field] = errorsForField;
     }
   }
   return Object.keys(errors).length ? errors : null;
